346abba3abf7b8f80eb39ad5f4163c859f0d4db8
AntOnAntOff <povanton@gmail.com>
2023-05-25 13:36:48 +0300

    refactors App and ContactForm files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,12 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 import ContactList from './ContactList';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
-    name: '',
-    number: ''
   }
-
-handleChange = e => {
-  const {name, value} = e.currentTarget;
-  
-this.setState({[name]: value});
-}
-
-handleAddContact = (e) => {
-  e.preventDefault();
-  const newContact = {
-    id: nanoid(),
-    name: this.state.name.trim(),
-    number: this.state.number.trim(),
-  };
-  this.setState(({ contacts }) => ({
-    contacts: [newContact, ...contacts],
-    name: '',
-    number: '',
-  }));
-};
 
 changeFilter = (e) => {
 this.setState({filter: e.currentTarget.value});
@@ -46,6 +18,12 @@ getFilterContact = () => {
   return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter));
 }
 
+addContact = (newContact) => {
+  this.setState((prevState) => ({
+    contacts: [newContact, ...prevState.contacts],
+  }));
+};
+
   render() {
     const {filter} = this.state;
     const filterContact = this.getFilterContact();
@@ -54,10 +32,7 @@ getFilterContact = () => {
       <div>
         <h1>Phonebook</h1>
       <ContactForm
-      onName={this.state.name}
-      onNumber={this.state.number}
-      onChangeContact={this.handleChange}
-      onSubmit={this.handleAddContact}
+      onAddContact={this.addContact}
       />
     
         <h2>Contacts</h2>
